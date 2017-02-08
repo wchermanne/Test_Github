6a5dc737235534dae7023ea6d42ed21feb0f8f3a
William Chermanne <william.chermanne@hotmail.com>
2017-02-08 15:59:28 +0100

    Deleted William
    Deleted William

--- main.swift
+++ main.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-let william = Personne( nom: "Chermanne",prenom: "William", sexe :"m", age: 21, adresse: "Stockholm")
 let noemie = Personne( nom: "Depré",prenom: "Noémie", sexe :"f", age: 19, adresse: "Charleroi")
 let robin = Personne( nom: "Van Lierde",prenom: "Robin", sexe :"m", age: 21, adresse: "Bruxelles")
 
