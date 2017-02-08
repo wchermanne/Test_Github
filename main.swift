9b9aa8b6ef47bb8b980734c418115d093fac7d89
William Chermanne <william.chermanne@hotmail.com>
2017-02-08 15:58:10 +0100

    Robin
    Added Robin

--- main.swift
+++ main.swift
@@ -10,6 +10,7 @@ import Foundation
 
 let william = Personne( nom: "Chermanne",prenom: "William", sexe :"m", age: 21, adresse: "Stockholm")
 let noemie = Personne( nom: "Depré",prenom: "Noémie", sexe :"f", age: 19, adresse: "Charleroi")
+let robin = Personne( nom: "Van Lierde",prenom: "Robin", sexe :"m", age: 21, adresse: "Bruxelles")
 
 william.affiche()
 
